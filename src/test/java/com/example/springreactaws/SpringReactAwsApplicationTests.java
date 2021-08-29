f54ab39aef4f772ab914c5654bfdc58630910898
mj <mu86@naver.com>
2021-08-29 16:51:10 +0900

    깃허브 액션 테스트

--- src/test/java/com/example/springreactaws/SpringReactAwsApplicationTests.java
+++ src/test/java/com/example/springreactaws/SpringReactAwsApplicationTests.java
@@ -6,14 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringReactAwsApplicationTests {
-
-	@Test
-	void githubActionTest1() {
-		Assertions.fail("test failed!");
-	}
-
-	@Test
-	void githubActionTest2() {
-		Assertions.fail("fail");
-	}
 }
